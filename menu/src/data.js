e4965d2959897eea2de7ccec043504056617518d
Mohamad Ayman Mslmani <ayma513@gmail.com>
2023-01-19 09:54:35 +0100

    new react project about Restaurant Menu

---
+++ menu/src/data.js
@@ -0,0 +1,81 @@
+export const items = [
+    {
+        id:Math.random(),
+        title:'Breakfast',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'12',
+        category:'breakfast',
+        imgUrl:'breakfast.png'
+    },
+    {
+        id:Math.random(),
+        title:'Lunch',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'14',
+        category:'lunch',
+        imgUrl:'lunch.png'
+    },
+    {
+        id:Math.random(),
+        title:'Dinner',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'15',
+        category:'dinner',
+        imgUrl:'dinner.png'
+    },
+    {
+        id:Math.random(),
+        title:'Dessert',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'9',
+        category:'dessert',
+        imgUrl:'dessert2.png'
+    },{
+        id:Math.random(),
+        title:'Breakfast',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'12',
+        category:'breakfast',
+        imgUrl:'breakfast.png'
+    },
+    {
+        id:Math.random(),
+        title:'Lunch',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'14',
+        category:'lunch',
+        imgUrl:'lunch2.png'
+    },
+    {
+        id:Math.random(),
+        title:'Dinner',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'15',
+        category:'dinner',
+        imgUrl:'dinner.png'
+    },
+    {
+        id:Math.random(),
+        title:'Dessert',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'9',
+        category:'dessert',
+        imgUrl:'dessert.png'
+    },
+    {
+        id:Math.random(),
+        title:'Lunch',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'14',
+        category:'lunch',
+        imgUrl:'lunch2.png'
+    },
+    {
+        id:Math.random(),
+        title:'Dessert',
+        description:'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+        price:'9',
+        category:'dessert',
+        imgUrl:'dessert.png'
+    },
+]
